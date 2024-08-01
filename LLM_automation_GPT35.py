757e5a194a89a042e62c168c95760af3e75420cf
MD Thamed Bin Zaman Chowdhury <zamanthamed@gmail.com>
2024-08-01 13:29:34 +0600

    Update LLM_automation_GPT35.py

--- LLM_automation_GPT35.py
+++ LLM_automation_GPT35.py
@@ -12,7 +12,7 @@ def create_data(description):
     load_dotenv()
 
     ### Set all api keys:
-    os.environ["OPENAI_API_KEY"]="sk-proj-ZB9b6Gn2FccVRsaL9WYfT3BlbkFJDpUpcoUwyR9LPoIJuAVl"
+    os.environ["OPENAI_API_KEY"]="ENTER YOUR API HERE"
 
 
     ### Create Prompt Template:
